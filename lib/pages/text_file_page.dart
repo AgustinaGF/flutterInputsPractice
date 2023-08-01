0e6b841803b74bf3084b58f639688163ee3dd7d4
AgustinaGF <agustinagomez385@gmail.com>
2023-08-01 12:54:55 -0300

    countries 166

--- lib/pages/text_file_page.dart
+++ lib/pages/text_file_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:inputs/constants/countries.dart';
+import 'package:inputs/models/country.dart';
 
 class TextFieldPage extends StatefulWidget {
   const TextFieldPage({super.key});
@@ -8,8 +10,34 @@ class TextFieldPage extends StatefulWidget {
 }
 
 class _TextFieldPageState extends State<TextFieldPage> {
+  late final List<Country> _countries;
+
+  @override
+  void initState() {
+    super.initState();
+
+    final countriesList = <Country>[];
+    _countries = countries
+        .map(
+          (e) => Country.fromJson(e),
+        )
+        .toList();
+    _countries = countriesList;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView.builder(
+        itemBuilder: (_, index) {
+          final country = _countries[index];
+          return ListTile(
+            title: Text(country.name),
+          );
+        },
+        itemCount: _countries.length,
+      ),
+    );
   }
 }
