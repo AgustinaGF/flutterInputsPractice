892498ef458939e153532aa55982f971550298b6
AgustinaGF <agustinagomez385@gmail.com>
2023-08-04 15:08:10 -0300

    search country 167

--- lib/pages/text_file_page.dart
+++ lib/pages/text_file_page.dart
@@ -11,32 +11,50 @@ class TextFieldPage extends StatefulWidget {
 
 class _TextFieldPageState extends State<TextFieldPage> {
   late final List<Country> _countries;
+  String _query = '';
 
   @override
   void initState() {
     super.initState();
 
-    final countriesList = <Country>[];
     _countries = countries
         .map(
           (e) => Country.fromJson(e),
         )
         .toList();
-    _countries = countriesList;
   }
 
   @override
   Widget build(BuildContext context) {
+    late final List<Country> filteredList;
+
+    if (_query.isEmpty) {
+      filteredList = _countries;
+    } else {
+      filteredList = _countries
+          .where(
+            (e) => e.name.toLowerCase().contains(_query.toLowerCase()),
+          )
+          .toList();
+    }
+
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: TextField(
+          onChanged: (text) {
+            _query = text;
+            setState(() {});
+          },
+        ),
+      ),
       body: ListView.builder(
         itemBuilder: (_, index) {
-          final country = _countries[index];
+          final country = filteredList[index];
           return ListTile(
             title: Text(country.name),
           );
         },
-        itemCount: _countries.length,
+        itemCount: filteredList.length,
       ),
     );
   }
