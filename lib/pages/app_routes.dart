02d22abd743d205b119f8304d7a84ec2b3200e60
AgustinaGF <agustinagomez385@gmail.com>
2023-08-09 15:51:42 -0300

    checkbox

--- lib/pages/app_routes.dart
+++ lib/pages/app_routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:inputs/pages/Keyboard_types_page.dart';
+import 'package:inputs/pages/check_box_page.dart';
 import 'package:inputs/routes.dart';
 import 'package:inputs/pages/text_file_page.dart';
 
@@ -7,5 +8,6 @@ Map<String, Widget Function(BuildContext)> get appRoutes {
   return {
     Routes.textField: (_) => const TextFieldPage(),
     Routes.keyboardTypes: (_) => const KeyboardTypesPage(),
+    Routes.checkbox: (_) => const CheckBoxPage(),
   };
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
+
 class Routes {
   Routes._();
 
   static const textField = '/text-field';
   static const keyboardTypes = '/keyboard-types';
+  static const checkbox = '/check-box';
 }
